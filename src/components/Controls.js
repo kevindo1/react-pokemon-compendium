dd9786f802f652d10be2084e2252e5ab24fd9eb4
Kevin Do <kevind0501@gmail.com>
2021-12-09 15:14:10 -0800

    added third party from mui.com

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 
 export default function Controls({
   query,
@@ -12,29 +18,37 @@ export default function Controls({
 }) {
   return (
     <div>
-      <input
+      <TextField
+        id="filled-basic"
         type="text"
         placeholder="Search Name"
-        label="Outlined"
-        variant="outlined"
+        label="Search Name"
+        variant="filled"
         value={query}
         onChange={(e) => {
           setQuery(e.target.value);
         }}
       />
-      <select value={order} onChange={(e) => setOrder(e.target.value)}>
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-      </select>
-      <select value={selectedTypes} onChange={(e) => setSelectedTypes(e.target.value)}>
-        <option value="All">All</option>
-        {types.map((type) => (
-          <option key={type} value={type}>
-            {type}
-          </option>
-        ))}
-      </select>
-      <button onClick={() => setLoading(true)}>Search</button>
+      <FormControl fullwidth>
+        <InputLabel>Sort Order</InputLabel>
+        <Select label="Sort Order" value={order} onChange={(e) => setOrder(e.target.value)}>
+          <MenuItem value={'asc'}>Ascending</MenuItem>
+          <MenuItem value={'desc'}>Descending</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullwidth>
+        <Select value={selectedTypes} onChange={(e) => setSelectedTypes(e.target.value)}>
+          <MenuItem value="All">All</MenuItem>
+          {types.map((type) => (
+            <MenuItem key={type} value={type}>
+              {type}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button variant="contained" onClick={() => setLoading(true)}>
+        Search
+      </Button>
     </div>
   );
 }
